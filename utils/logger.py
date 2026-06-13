f87d68e7e47955d5af36d0b6480107a215d59b12
dipagsiXraunak <dipagsisharma@gmail.com>
2026-06-13 22:09:12 +0530

    Delete utils directory

--- utils/logger.py
+++
@@ -1,8 +0,0 @@
-from config import LOGGER_ID
-
-async def send_log(client, text: str):
-    if LOGGER_ID:
-        try:
-            await client.send_message(LOGGER_ID, text)
-        except Exception as e:
-            print(f"Failed to send log: {e}")
